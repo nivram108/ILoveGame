19b9b84f20cf90f312d9b02e398b322496a16ea8
poopoomarvin <hchs010077@gmail.com>
2019-05-19 13:52:37 +0800

    change ip

--- simple_client.py
+++ simple_client.py
@@ -29,8 +29,8 @@ class_type = 'marvin0318'
 def client_program():
 	print(socket.gethostname())
 	#host = '10.0.0.164'
-	#host = '10.0.0.2'
-	host = '127.0.0.1'
+	host = '10.0.0.2'
+	#host = '127.0.0.1'
 	port = 5010# socket server port number
 	#client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # instantiate
 	client_socket = socket.socket()  # instantiate
